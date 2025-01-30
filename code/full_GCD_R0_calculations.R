47cc3116ece45ab51f15fd5a1da8f63b10152e52
Kyle Dahlin <kydahlin@gmail.com>
2025-01-29 23:16:05 -0500

    set up empirical GCD - R0 calculations

--- code/full_GCD_R0_calculations.R
+++ code/full_GCD_R0_calculations.R
@@ -30,6 +30,7 @@ library(matlib)
 R0_calc <- function(A, v_alpha, LambdaB, LambdaH, BetaB, BetaH) {
   # Calculates R0 as a function of the phase-type distribution parameters
   # and specific transmission formulation
+  
 }
 
 # Calculate theta, biting process duration
@@ -68,7 +69,7 @@ varPhi = 300 / 1440 # on average 3 eggs per female per day
 # Set range of GCD values to consider
 # Note: GCD = biting duration + oviposition duration + resting duration
 #       Only theta = biting duration will be varied
-resolution = 100
+resolution = 1001
 
 theta_min = 8 * 60 # minimum biting duration of 8 hours
 theta_max = 20 * 24 * 60 # maximum biting duration of 20 days
@@ -87,13 +88,106 @@ b_vec = 1/theta_vec
 ## For each theta value...
 
 ## Generate 100 samples from a lognormal distribution with mean theta, variance 1...
+samples <- tibble(theta = theta_vec) %>% 
+  expand_grid(tibble(n = 1:resolution)) %>% 
+  rowwise() %>% 
+  mutate(sample = rlnorm(1, meanlog = log(theta), sdlog = 1))
 
 ## Specify initial guess A and v_alpha...
 ## Should be dimension 3. Use the same for all theta values (for now)
+set.seed(24060)
+v_alpha_in = rgamma(3, 1)
+v_alpha_in = v_alpha_in/sum(v_alpha_in)
+
+# Initialize data frame
+out_df = as_tibble(matrix(
+  nrow = 0, 
+  ncol = 3, 
+  dimnames = list(NULL, c("n", "alpha", "A"))
+))
+
 
 ## Run EM algorithm to obtain best-fit A and v_alpha...
+library(mapfit)
+out_df = data.frame()
+for (theta_val in theta_vec) {
+  sample_in = filter(samples, theta == theta_val)$sample
+  set.seed(24060) # to ensure same initial state
+  out = phfit.point(ph = ph(3), 
+                    x = sample_in)
+  
+  out_df = rbind(out_df,
+                 tibble(theta = theta_val, alpha = list(out$alpha), A = c(out$Q)))
+}
+saveRDS(out_df, "data/EM_fits_to_lognormal.rds")
+
+out_df = readRDS("data/EM_fits_to_lognormal.rds")
+
 
 ## [Sanity check] Calculate theta_calc(A, v_alpha) and compare to theta
+
+# For some chosen values of theta, plot pdfs of lognormal and fitted PH
+library(PhaseTypeR)
+library(gridExtra)
+library(cowplot)
+library(latex2exp)
+
+test_theta_vals = c(min(samples$theta), median(samples$theta), max(samples$theta))
+
+test_df = tibble(test_theta = test_theta_vals) %>% 
+  rowwise() %>% 
+  mutate(ph_model = list(phfit.point(ph = ph(3), 
+                                     x = filter(samples, theta == test_theta)$sample)$model),
+         row_num = row_number()) %>% 
+  group_by(row_num)
+
+x_maxs = c(2750, 75000, 200000)
+scale_factors = c(1, 10, 10)
+legend_list = c("none", "none", "legend")
+
+plot_list = list()
+
+for (i in 1:length(test_df)) {
+  test_theta_val = test_df$test_theta[i]
+  x_max = x_maxs[i]
+  temp_plot = ggplot(data.frame(x_vals = seq(0, x_max, length.out = 1000)), aes(x = x_vals)) +
+    stat_function(fun = dphase, args = list(ph = test_df$ph_model[[i]]),
+                  aes(color = "PH"),
+                  lwd = 1
+    ) +
+    stat_function(fun = dlnorm, args = list(mean = log(test_theta_val), sd = 1), 
+                  aes(color = "lognormal"),
+                  lwd = 1
+    ) +
+    scale_x_continuous(name = "Days", breaks = seq(0, x_max, by = scale_factors[i]*1440), labels = seq(0, x_max / (scale_factors[i]*1440), by = 1)) + 
+    # scale_x_continuous(name = "Days", trans = ~ . / 1440) +
+    scale_y_continuous(name = "") +
+    guides(
+      y = "none"
+      ) +
+    scale_color_discrete(name = "Distribution") +
+    ggtitle(TeX(paste0("$\\theta \\approx ", round(test_theta_val/1440, 2)," days"))) +
+    theme_minimal(18) +
+    theme(
+      legend.position = "right",  # Legend positioned to the right
+      legend.key.size = unit(1, "cm")  # Adjust legend key size
+    )
+  
+  plotgrob = ggplotGrob(temp_plot)
+  legend_index <- which(sapply(plotgrob$grobs, function(x) x$name) == "guide-box")
+  legend <- plotgrob$grobs[[legend_index]]
+  
+  # panel_index <- which(plotgrob$layout$name == "panel")
+  # plot_panel <- gtable::gtable_filter(plotgrob, "panel")
+  
+  plotgrob$grobs[[legend_index]] <- nullGrob()
+  
+  plot_list[[i]] = temp_plot + theme(legend.position = "none")
+}
+
+# plot_only = 
+
+all_plots <- grid.arrange(plot_list[[1]], plot_list[[2]], plot_list[[3]], legend, nrow = 1, widths = (3, 1))
 
 ## Determine LambdaH, LambdaB, BetaH, BetaB from A...
 
