b8a2e4d366013f09df2f207c6e23e808a453a389
Kyle Dahlin <kydahlin@gmail.com>
2025-08-26 14:01:51 -0400

    checking R0 calculations

--- code/julia/GCD_R0_num_calc.jl
+++ code/julia/GCD_R0_num_calc.jl
@@ -114,10 +114,98 @@ function R0_func(B_vals_in)
 
         sum_B_star = sum(B_star)
 
-        R02 = (1 / (gH + muH)) * transpose(one_vec_four) * betaH_mat * LambdaH * GammaI * tauE * betaV_mat * LambdaV * (B_star / sum_B_star)
+        R02 = (KH / sum_B_star) * transpose(one_vec_four) * betaH_mat * LambdaH * GammaI * tauE * (1/KH) * (betaV_mat * LambdaV * B_star) * (1 / (gH + muH))
         R0 = sqrt(R02[1])
         return(R0)
     end
+end
+
+function repnums_func(B_vals_in)
+
+    (lQ, lL, lP, lG, sigma, pQ, pL, pP, pG) = B_vals_in 
+
+    one_vec_four = [1.0f0; 1.0f0; 1.0f0; 1.0f0]
+    alpha_vec_four = [1.0f0; 0.0f0; 0.0f0; 0.0f0]
+
+    # Define subintensity matrices
+    A_mat = [
+        -lQ+(1-pQ)*lQ       pQ*lQ               0f0   0f0   
+        (1-sigma)*(1-pL)*lL -lL+sigma*(1-pL)*lL pL*lL 0f0 
+        (1-sigma)*(1-pP)*lP sigma*(1-pP)*lP     -lP   pP*lP
+        (1-sigma)*(1-pG)*lG sigma*(1-pG)*lG     0f0   -lG
+    ]
+
+    tau = transpose(-A_mat * one_vec_four) * inv(mu * I - transpose(A_mat)) * alpha_vec_four
+    tau = tau[1]
+    rho = (gV / (mu + gV)) * (gR / (mu + gR)) * tau
+    nG = 1.0f0 / (1.0f0 - rho)
+    # Basic offspring number
+    N_offspring = tau * (varPhi / (mu + gV)) * (rhoJ / (rhoJ + muJ)) * nG
+
+    if N_offspring < 1
+            RHV_scalar = 0.0f0
+            RVH_scalar = 0.0f0
+            R0 = 0.0f0
+    else
+        # Distribution of mosquitoes across states at equilibrium
+        B_prefactor = rhoJ * KJ *(N_offspring - 1) * nG / N_offspring
+        temp_inv = inv(mu * I - transpose(A_mat))
+        B_postfactor = temp_inv * alpha_vec_four
+
+        B_star = B_prefactor * B_postfactor
+        # V_star = r / (mu + gV)
+        KB = sum(B_star)
+
+        betaH_mat = zeros(Float64, 4,4); betaV_mat = zeros(Float64, 4,4); LambdaH = zeros(Float64, 4,4); LambdaV = zeros(Float64, 4,4)
+        # Rate of contact is rate of entrance into transmission compartments
+        LambdaH[3,3] = lP * KB / KH
+        LambdaV[4,4] = lG
+        betaH_mat[3,3] = bH
+        betaV_mat[4,4] = bB
+
+        # spec_mat = alpha_vec_four * transpose(-A_mat * one_vec_four)
+
+        # GammaI = inv(mu * I - transpose(A_mat) + (gV / (mu + gV)) * (gR / (mu + gR)) * spec_mat)
+        # GammaE = inv((eta + mu) * I - transpose(A_mat) + (gR / (mu + gR + eta)) * (gV / (mu + gV + eta)) * spec_mat)
+
+        # complicated_probability = (gV/(mu+gV)) * ((eta / (mu+gV+eta)) * (gR/(mu+gR+eta)) + (eta/(mu+gR+eta) * (gR/(mu+gR))))
+        # tauE = (eta * I + complicated_probability * spec_mat) * GammaE
+
+        sum_B_star = sum(B_star)
+
+        FVH = [zeros(1,4) ((KH / (sum_B_star)) * transpose(one_vec_four) * betaH_mat * LambdaH);
+            zeros(1,4) zeros(1,4)]
+        FHV = [(1/KH) * (betaV_mat * LambdaV * B_star) zeros(4,1);
+                zeros(4,1)       zeros(4,1)]
+
+        VH = [(muH+gH) 0;
+            -gH      muH]
+        VBB = [(mu+eta)*I-transpose(A_mat) zeros(4,4);
+            (-eta * I)                  (mu*I-transpose(A_mat))]
+        VVR = [(mu+gV+eta) 0       0           0;
+            -eta        (mu+gV) 0           0;
+            -gV         0       (mu+gR+eta) 0;
+            0          -gV      -eta        (mu+gR)]
+        VRB = [zeros(4,1) zeros(4,1) -gR*alpha_vec_four zeros(4,1);
+            zeros(4,1) zeros(4,1) zeros(4,1)         -gR*alpha_vec_four]
+        VBV = [-transpose(-A_mat * one_vec_four) zeros(1,4);
+            zeros(1,4)                        -transpose(-A_mat * one_vec_four);
+            zeros(1,4)                        zeros(1,4) ;
+            zeros(1,4)                        zeros(1,4)]
+        big_inv = inv(VBB - VRB * inv(VVR) * VBV)
+
+        # Reproduction number matrices
+        RVH = FVH * big_inv
+        RHV = FHV * inv(VH)
+
+        RVH_scalar = RVH[1,4]
+        RHV_scalar = RHV[4,1]
+
+        R02 = RVH_scalar * RHV_scalar
+        R0 =  sqrt(R02)
+    end
+
+    return([N_offspring, RVH_scalar, RHV_scalar, R0])
 end
 
 # Define parameter ranges
